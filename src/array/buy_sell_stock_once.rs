667fa952a3266e9766ca2d2796122ff5d7d9e4e9
karim <karim.elmougi@gmail.com>
2021-03-21 13:24:22 -0400

    Add 5.6 Buy And Sell Stock Once

---
+++ src/array/buy_sell_stock_once.rs
@@ -0,0 +1,35 @@
+use std::cmp::max;
+
+pub fn buy_sell_stock_once(array: &[i32]) -> i32 {
+    if array.is_empty() {
+        return 0;
+    }
+
+    let mut minima = array.first().unwrap();
+    let mut maxima = array.first().unwrap();
+    let mut max_delta = 0;
+
+    for price in array {
+        if price < minima {
+            max_delta = max(max_delta, maxima - minima);
+            minima = price;
+            maxima = price;
+        } else {
+            maxima = price;
+        }
+    }
+
+    max(max_delta, maxima - minima)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let a = vec![310, 315, 275, 295, 260, 270, 290, 230, 255, 250];
+        let max_profit = buy_sell_stock_once(&a);
+        assert_eq!(max_profit, 30);
+    }
+}
--- src/array/mod.rs
+++ src/array/mod.rs
@@ -1,3 +1,4 @@
+pub mod buy_sell_stock_once;
 pub mod dutch_national_flag;
 pub mod increment_integer;
 pub mod multiply_integers;
